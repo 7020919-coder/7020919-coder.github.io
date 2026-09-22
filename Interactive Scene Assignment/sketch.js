9ed8820c6078d642b3c4ed810159096cf7d527e6
Dara <7020919@spsd.sk.ca>
2026-09-22 14:12:03 -0600

    Sept 22ns, 2026
    worked further on background

--- Interactive Scene Assignment/sketch.js
+++ Interactive Scene Assignment/sketch.js
@@ -11,12 +11,41 @@ var groundY = 420;
 var groundWidth = 1000;
 var groundHeight = 100;
 
+var circleX = 70;
+var circleY = 70;
+var circleR = 100;
+
+var houseX = 500;
+var houseY = 240;
+var houseWidth = 230;
+var houseHeight = 200;
+
+var tx1 = 500;
+var ty1 = 240;
+var tx2 = 610;
+var ty2 = 100;
+var tx3 = 730;
+var ty3 = 240;
+
+var bushX = 50;
+var bushY = 470;
+var bushR = 200;
+
+function bushes(){
+  circle(bushX, bushY, bushR);
+  circle(bushX+150, bushY, bushR);
+  circle(bushX+300, bushY, bushR);
+  circle(bushX+450, bushY, bushR);
+  circle(bushX+600, bushY, bushR);
+  circle(bushX+750, bushY, bushR);
+}
+
 async function setup() {
   createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
-  background(225, 148, 72);
+  background(141, 196, 252);
   
   //Floor
   let g = color(95, 70, 44);
@@ -25,8 +54,32 @@ function draw() {
   rect(groundX, groundY, groundWidth, groundHeight);
 
   // sun/moon
-  let s = color(236, 43, 43);
+  let s = color(250, 241, 161);
   fill(s);
   noStroke();
+  circle(circleX, circleY, circleR);
   
+  // House
+  let h = color(247, 147, 247);
+  fill(h);
+  noStroke();
+  rect(houseX, houseY, houseWidth, houseHeight);
+
+  let r = color(75, 60, 75);
+  fill(r);
+  triangle(tx1, ty1, tx2, ty2, tx3, ty3);
+  rect(houseX+75, houseY+50, houseWidth/3, houseHeight-20)
+
+  //bushes
+  let b = color(21, 131, 25);
+  fill(b);
+  bushes();
+  
+}
+
+function ghost(){
+  let g = color(255,255,255);
+  fill(g);
+  noStroke();
+  circle(2,2,60);
 }
